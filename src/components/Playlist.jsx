377e950090771d33ad64d4d44a8f64a030375c29
Luis Carlos Garzón <luis@luiskaizen.com>
2025-06-11 05:55:09 -0500

    Lint code and apply fixes

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Tracklist from './Tracklist'
 
 function Playlist({tracks, removeItem}) {
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 function Track({info, button, updatePlaylistItems, removeItem}){
     //console.log(info);
